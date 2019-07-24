b1d1be9aa4e40d637375482af590cb880c286b53
MatanHa <matanhayun94@gmail.com>
2019-07-24 15:10:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('See You World!')
+  response.send('See You good World!')
 })
 
 app.listen(app.get('port'), function() {
